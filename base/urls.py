3f375ba6dc8db7ab401cc74afd41a1ee18a8c218
Pranjal Agarwal <pranjalagarwal4311@gmail.com>
2022-09-22 12:24:26 +0530

    Deleting env and random files

--- base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('documentation/', views.documentation, name="documentation"),
-    path('code/', views.code, name="Get Code"),
-]
