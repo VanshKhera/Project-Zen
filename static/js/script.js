0253fbf0b1f1ca400ce8940f1fa135b58369e624
Pranjal Agarwal <pranjalagarwal4311@gmail.com>
2022-09-26 10:33:55 +0530

    Adding animation to ham, not successful.

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,11 @@ const menu = document.getElementById("h-menu");
 
 ham.addEventListener("click", function (e) {
   if (menu.style.display == "none"){
-      menu.style.display = "block"
+      menu.style.display = "block";
+      menu.classList.add('menu_active');
   }
   else{
-    menu.style.display = "none"
+    menu.style.display = "none";
+    menu.classList.remove('menu_active');
   }
 })
-
-function show(id){
-  var modal = document.getElementById(id);
-  modal.style.visibility="visible";
-}
